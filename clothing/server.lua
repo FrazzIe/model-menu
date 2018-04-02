bf973de31c8b663b9fa622efc77b865962e3b754
Corey McCown <coreymccown13@gmail.com>
2018-04-02 10:21:11 -0400

    Update server.lua
    Added ESX. Switched from .txt storage to MySQL.

--- clothing/server.lua
+++ clothing/server.lua
@@ -1,30 +1,43 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
 models = {}
+
 RegisterServerEvent("clothes:firstspawn")
 AddEventHandler("clothes:firstspawn",function()
-	local source = source
-	local identifier = getID("steam",source)
-	print(identifier)
-	if models[identifier] then
-		TriggerClientEvent("clothes:spawn", source, models[identifier])
-	else
-		local default_models = {1413662315,-781039234,1077785853,2021631368,1423699487,1068876755,2120901815,-106498753,131961260,-1806291497,1641152947,115168927,330231874,-1444213182,1809430156,1822107721,2064532783,-573920724,-782401935,808859815,-1106743555,-1606864033,1004114196,532905404,1699403886,-1656894598,1674107025,-88831029,-1244692252,951767867,1388848350,1090617681,379310561,-569505431,-1332260293,-840346158}
-		models[identifier] = {
-			model = default_models[math.random(1,tonumber(#default_models))],
-			new = true,
-			clothing = {drawables = {0,0,0,0,0,0,0,0,0,0,0,0},textures = {2,0,1,1,0,0,0,0,0,0,0,0},palette = {0,0,0,0,0,0,0,0,0,0,0,0}},
-			props = {drawables = {-1,-1,-1,-1,-1,-1,-1,-1}, textures = {-1,-1,-1,-1,-1,-1,-1,-1}},
-			overlays = {drawables = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, opacity = {1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0}, colours = {{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0}}},
-		}
-		saveModels()
-		TriggerClientEvent("clothes:spawn", source, models[identifier])
+	local _source = source
+    local xPlayer  = ESX.GetPlayerFromId(_source)
+	if xPlayer ~= nil then
+		local identifier = xPlayer.identifier
+		MySQL.Async.fetchAll("SELECT * FROM users WHERE identifier = @identifier", {
+		  ['@identifier'] = identifier
+		},function(result)
+			local user      = result[1]
+			local skin   = user['skin']
+			
+			if skin == "" or skin == nil then
+				local default_models = {1413662315,-781039234,1077785853,2021631368,1423699487,1068876755,2120901815,-106498753,131961260,-1806291497,1641152947,115168927,330231874,-1444213182,1809430156,1822107721,2064532783,-573920724,-782401935,808859815,-1106743555,-1606864033,1004114196,532905404,1699403886,-1656894598,1674107025,-88831029,-1244692252,951767867,1388848350,1090617681,379310561,-569505431,-1332260293,-840346158}
+				models[identifier] = {
+					model = default_models[math.random(1,tonumber(#default_models))],
+					new = true,
+					clothing = {drawables = {0,0,0,0,0,0,0,0,0,0,0,0},textures = {2,0,1,1,0,0,0,0,0,0,0,0},palette = {0,0,0,0,0,0,0,0,0,0,0,0}},
+					props = {drawables = {-1,-1,-1,-1,-1,-1,-1,-1}, textures = {-1,-1,-1,-1,-1,-1,-1,-1}},
+					overlays = {drawables = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, opacity = {1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0}, colours = {{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0},{colourType = 0, colour = 0}}},
+				}
+				TriggerClientEvent("clothes:spawn", _source, models[identifier])
+			else
+				models[identifier] = json.decode(skin)
+				TriggerClientEvent("clothes:spawn", _source, json.decode(skin))
+			end
+		end)
 	end
 end)
 
 RegisterServerEvent("clothes:spawn")
 AddEventHandler("clothes:spawn",function()
-	local source = source
-	local identifier = getID("steam",source)
-	TriggerClientEvent("clothes:spawn", source, models[identifier])
+	local _source = source
+	local identifier = getID("steam",_source)
+	TriggerClientEvent("clothes:spawn", _source, models[identifier])
 end)
 
 RegisterServerEvent("clothes:loaded")
@@ -34,21 +47,19 @@ end)
 
 RegisterServerEvent("clothes:save")
 AddEventHandler("clothes:save",function(player_data)
-	local source = source
-	local identifier = getID("steam",source)
-	models[identifier] = player_data
-	saveModels()
+	local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+	if xPlayer ~= nil then
+		local identifier = xPlayer.identifier
+		MySQL.Async.execute("UPDATE users SET skin = @skin WHERE identifier = @identifier", {
+		  ['@skin'] = tostring(json.encode(player_data)),
+		  ['@identifier'] = identifier
+		})
+		models[identifier] = player_data
+	end
+
 	-- Give weapons etc
 end)
-
-function loadModels()
-    models = LoadResourceFile(GetCurrentResourceName(), "models.txt") or "[]"
-    models = json.decode(models)
-end
-
-function saveModels()
-	SaveResourceFile(GetCurrentResourceName(), "models.txt", json.encode(models), -1)
-end
 
 function getID(type, source)
     for k,v in ipairs(GetPlayerIdentifiers(source)) do
@@ -62,5 +73,3 @@ function getID(type, source)
     end
     return nil
 end
-
-loadModels()
